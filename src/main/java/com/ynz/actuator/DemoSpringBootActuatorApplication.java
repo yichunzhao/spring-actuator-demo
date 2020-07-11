7abb360dbdb90e391d4a3f2295513b957ea857fe
yichunzhao <zhaoyichun@hotmail.com>
2020-07-12 01:17:19 +0200

    Initial commit

---
+++ src/main/java/com/ynz/actuator/DemoSpringBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootActuatorApplication.class, args);
+	}
+
+}
