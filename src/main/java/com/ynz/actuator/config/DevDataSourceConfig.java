58c5ef239db476f8df0f6e102e0ea7d2fb6058a9
yichunzhao <zhaoyichun@hotmail.com>
2020-07-12 06:24:22 +0200

    connecting to cloud PostgreSql databases

---
+++ src/main/java/com/ynz/actuator/config/DevDataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.ynz.actuator.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DevDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("test");
+        return dataSourceBuilder.build();
+    }
+}
