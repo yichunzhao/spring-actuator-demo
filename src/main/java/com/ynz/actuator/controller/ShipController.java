28dd72c53e7e6840b07ae6327a28b5f6f1099a8c
yichunzhao <zhaoyichun@hotmail.com>
2020-07-15 15:38:07 +0200

    Extending the matrix to a custom usage; measuring time cost of invoking an API, or counting how many times of API having been invoked.

--- src/main/java/com/ynz/actuator/controller/ShipController.java
+++ src/main/java/com/ynz/actuator/controller/ShipController.java
@@ -3,7 +3,9 @@ package com.ynz.actuator.controller;
 import com.ynz.actuator.domain.Ship;
 import com.ynz.actuator.exceptions.NotFoundException;
 import com.ynz.actuator.repository.ShipRepository;
-import lombok.RequiredArgsConstructor;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,25 +14,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ships")
-@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ShipController {
     private final ShipRepository shipRepository;
+    private final MeterRegistry registry;
 
-    @GetMapping("")
+    private final Counter greetingCounter;
+
+    public ShipController(ShipRepository shipRepository, MeterRegistry registry) {
+        this.shipRepository = shipRepository;
+        this.registry = registry;
+        greetingCounter = Counter.builder("api.greeting").register(registry);
+    }
+
+    @GetMapping("greeting")
+    public String greeting() {
+        greetingCounter.increment();
+        return "hello, find ships.";
+    }
+
+    @GetMapping("/ships")
+    @Timed(value = "api.getAllShips")
     public ResponseEntity<List<Ship>> findAllShips() {
         List<Ship> ships = new ArrayList<>();
         shipRepository.findAll().forEach(ship -> ships.add(ship));
         return new ResponseEntity(ships, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/ships/{id}")
     public ResponseEntity<Ship> findShipById(@PathVariable("id") int id) {
         Ship found = getShipById(id);
         return new ResponseEntity(found, HttpStatus.FOUND);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/ships/{id}")
     public ResponseEntity deleteShipById(@PathVariable("id") int id) {
         getShipById(id);
         shipRepository.deleteById(id);
